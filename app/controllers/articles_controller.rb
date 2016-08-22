bf339654aec2ff11e346e1d17e77a8d626c6455d
allenhu08 <hhh.allen08@gmail.com>
2016-08-22 21:56:41 +0000

    second model - comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,19 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @articles = Article.all
   end
@@ -19,6 +32,12 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
